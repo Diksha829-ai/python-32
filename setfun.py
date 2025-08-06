b67aee19e119bf25356aa8cbb47a24b15b71fd58
Diksha829_ai <nikamdiksha897@gmail.com>
2025-08-06 15:58:18 +0530

    add intersection and union

--- setfun.py
+++ setfun.py
@@ -1,4 +1,4 @@
-fruits={"banana", "apple", "cherry", "mango", "orange"}
+fruits={"banana", "apple", "cherry", "mango", "orange","potato"}
 # Display the set
 print("Fruits Set:", fruits)
 
@@ -26,3 +26,13 @@ print("Is 'cherry' in fruits?", "cherry" in fruits)
 # Length of the set
 print("Length of fruits set:", len(fruits))
 
+#union of sets
+vegetables = {"carrot", "potato", "tomato", "cabbage", "spinach"}
+print("Vegetables Set:", vegetables)
+# Union of two sets
+combined_set = fruits.union(vegetables)
+print("Combined Set (Fruits ∪ Vegetables):", combined_set)  
+
+# Intersection of two sets
+intersection_set = fruits.intersection(vegetables)
+print("Intersection Set (Fruits ∩ Vegetables):", intersection_set)
