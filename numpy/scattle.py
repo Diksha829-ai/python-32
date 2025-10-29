a1e10b277f1a46022e1fa78786ca3a41e43e4e7a
Diksha829_ai <nikamdiksha897@gmail.com>
2025-10-29 15:20:32 +0530

    new

--- numpy/scattle.py
+++ numpy/scattle.py
@@ -6,7 +6,7 @@ weight = [45, 50, 55, 65, 70, 75, 85]
 
 # Create scatter plot
 plt.figure()
-plt.scatter(height, weight)  # No specific colors
+plt.scatter(height, weight) 
 plt.xlabel("Height (cm)")
 plt.ylabel("Weight (kg)")
 plt.title("Height vs Weight Scatter Plot")
